95fb8bc5d5e5589e2ea0a31a2bd8e82d9d712727
kenash-dev <44662307+kenash-dev@users.noreply.github.com>
2021-02-03 00:06:18 +1100

    Set up database (#14)
    * create a database project
    
    * Add Dbup project
    
    * Add connection string to db
    
    * Add appsettings.json
    
    * Add DbUp project to the solution.
    
    * Add appsettings environment, other changes as per code review.
    
    * remove production and development appsettings.json
    
    * Indentation and white spaces
    
    Co-authored-by: Michael Tran <mtran45@gmail.com>

---
+++ src/MovieSellerDb/Program.cs
@@ -0,0 +1,59 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace MovieSellerDb
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            IConfiguration Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            InitDatabase(Configuration);
+
+            return 0;
+        }
+
+        private static int InitDatabase(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DevDatabase");
+            EnsureDatabase.For.SqlDatabase(connectionString);
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .WithTransaction()
+                    .LogToConsole()
+                    .Build();
+
+            if (upgrader.IsUpgradeRequired())
+            {
+                var result = upgrader.PerformUpgrade();
+                if (!result.Successful)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(result.Error);
+                    Console.ResetColor();
+                    #if DEBUG
+                    Console.ReadLine();
+                    #endif
+                    return -1;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Success!");
+                Console.ResetColor();
+
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/MovieSellerDb/Scripts/001-CreateDatabase.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[user]
+(
+	[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(200) NOT NULL, 
+    [LastName] NVARCHAR(100) NOT NULL, 
+)
